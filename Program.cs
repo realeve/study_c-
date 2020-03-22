c1cc21fcabae3fc5728ff5aadc6b80cf8dab10e4
realeve <realeve@qq.com>
2020-03-23 00:07:58 +0800

    :memo: init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+public class Person
+{
+    public string strFirst { get; }
+    public string strLast { get; }
+
+    public Person(string first, string last)
+    {
+        strFirst = first;
+        strLast = last;
+    }
+
+    public override string ToString() => $"{strFirst} {strLast}";
+    public string AllCaps() => ToString().ToUpper();
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var p = new Person("li", "bin");
+        WriteLine("username:" + p.AllCaps());
+        WriteLine("user:" + p);
+    }
+}
---
+++ src/reactangle.cs
@@ -0,0 +1,34 @@
+using System;
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        double length;
+        double width;
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("length:{0}", length);
+            Console.WriteLine("width:{0}", width);
+            Console.WriteLine("area:{0}", GetArea());
+        }
+    }
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
